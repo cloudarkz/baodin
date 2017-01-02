2ef62d31f3f2675e8c5f63a6520a1e205aa80287
cloudarkz <brian_ngin@hotmail.com>
2017-01-02 18:17:12 +0800

    First commit: Hello World Android App

---
+++ app/src/main/java/com/jtb/baodin/baodin/MainActivity.java
@@ -0,0 +1,13 @@
+package com.jtb.baodin.baodin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
