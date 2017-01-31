53dee818ce0b0c89fd40f93a6b59d161f66895cb
jyonguwern <jyonguwern@gmail.com>
2017-02-01 00:39:57 +0800

    Implemented new Fragment Class
    - created UserProfile Fragment Class
    - hooked up fragment to navbar
    - created callback function to mainactivity
    
    note:
    4 tabs are using the same fragment class temporarily

--- app/src/main/java/com/jtb/baodin/MainActivity.java
+++ app/src/main/java/com/jtb/baodin/MainActivity.java
@@ -1,64 +1,111 @@
 package com.jtb.baodin;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.IdRes;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnTabReselectListener;
 import com.roughike.bottombar.OnTabSelectListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements UserProfileFragment.OnUserProfileFragmentInteraction {
 
-    private TextView textView;
+    private UserProfileFragment mBrowseFragment;
+    private UserProfileFragment mFavoritesFragment;
+    private UserProfileFragment mAppointmentFragment;
+    private UserProfileFragment mUserProfileFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Intent intent = new Intent(this, LoginActivity.class);
-        //startActivity(intent);
+        mBrowseFragment = UserProfileFragment.newInstance(getString(R.string.bottombar_tab_browse), getString(R.string.bottombar_tab_browse));
+        mFavoritesFragment = UserProfileFragment.newInstance(getString(R.string.bottombar_tab_favorites), getString(R.string.bottombar_tab_favorites));
+        mAppointmentFragment = UserProfileFragment.newInstance(getString(R.string.bottombar_tab_appointments), getString(R.string.bottombar_tab_appointments));
+        mUserProfileFragment = UserProfileFragment.newInstance(getString(R.string.bottombar_tab_userprofile), getString(R.string.bottombar_tab_userprofile));
 
-        textView = (TextView) findViewById(R.id.textView);
+ //       Intent intent = new Intent(this, LoginActivity.class);
+ //       startActivity(intent);
 
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.contentContainer, mBrowseFragment, mBrowseFragment.getTag());
+        fragmentTransaction.commit();
+
+        // BottomBar Listeners for swapping view fragments
         BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
         bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
             @Override
-            public void onTabSelected(@IdRes int tabId) {
-                if (tabId == R.id.tab_browse) {
-                    textView.setText("browse");
-                }
-                else if (tabId == R.id.tab_favorites) {
-                    textView.setText("favorites");
-                }
-                else if (tabId == R.id.tab_appointments) {
-                    textView.setText("appointments");
-                }
-                else if (tabId == R.id.tab_userprofile) {
-                    textView.setText("user profile");
-                }
+            public void onTabSelected(@IdRes int tabID) {
+                swapFragment(tabID);
             }
         });
-
         bottomBar.setOnTabReselectListener(new OnTabReselectListener() {
             @Override
-            public void onTabReSelected(@IdRes int tabId) {
-                if (tabId == R.id.tab_browse) {
-                    textView.setText("browse reselected");
-                }
-                else if (tabId == R.id.tab_favorites) {
-                    textView.setText("favorites reselected");
-                }
-                else if (tabId == R.id.tab_appointments) {
-                    textView.setText("appointments reselected");
-                }
-                else if (tabId == R.id.tab_userprofile) {
-                    textView.setText("user profile reselected");
-                }
+            public void onTabReSelected(@IdRes int tabID) {
             }
         });
+    }
+
+    /**
+     * Description:
+     * Handles the swapping between different view fragments.
+     *
+     * @param tabID int of the tab selected on the BottomBar
+     */
+    private void swapFragment(int tabID) {
+        Fragment fragment = null;
+
+        switch (tabID) {
+            case R.id.tab_browse:
+                fragment = mBrowseFragment;
+                updateActionbarText(getString(R.string.bottombar_tab_browse));
+                break;
+            case R.id.tab_favorites:
+                fragment = mFavoritesFragment;
+                updateActionbarText(getString(R.string.bottombar_tab_favorites));
+                break;
+            case R.id.tab_appointments:
+                fragment = mAppointmentFragment;
+                updateActionbarText(getString(R.string.bottombar_tab_appointments));
+                break;
+            case R.id.tab_userprofile:
+                fragment = mUserProfileFragment;
+                updateActionbarText(getString(R.string.bottombar_tab_userprofile));
+                break;
+        }
+
+        if (fragment != null) {
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.contentContainer, fragment, fragment.getTag());
+            fragmentTransaction.commit();
+        }
+    }
+
+    /**
+     * Description:
+     * Updates the Actionbar title text when fragments are swapped.
+     *
+     */
+    private void updateActionbarText(CharSequence text) {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle(text);
+        }
+    }
+
+    /**
+     * Description:
+     * Callback for user interaction on the User Profile Fragment.
+     *
+     */
+    public void onUserProfileFragmentInteraction(String string) {
+        updateActionbarText(string);
     }
 }
